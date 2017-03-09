71d0dcf6a9ffd520b8a73ca49df600992261e660
Winnie Quinn <me@winniequinn.com>
2017-03-08 20:29:06 -0500

    Factored out ToolbarActivity and fixed relative layout

--- app/src/main/java/org/nypl/linebylinedemo/MainActivity.kt
+++ app/src/main/java/org/nypl/linebylinedemo/MainActivity.kt
@@ -1,19 +1,16 @@
 package org.nypl.linebylinedemo
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.util.TypedValue
 import android.view.Menu
 import android.view.MenuItem
 import android.view.MenuItem.SHOW_AS_ACTION_IF_ROOM
 import android.view.MotionEvent
-import android.view.ViewGroup
+import android.view.View
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.webkit.WebView
 import android.widget.RelativeLayout
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ToolbarActivity() {
 
     lateinit var webView: WebView
     lateinit var previousMenuItem: MenuItem
@@ -21,47 +18,20 @@ class MainActivity : AppCompatActivity() {
 
     init {
         WebView.setWebContentsDebuggingEnabled(true)
-    }
-
-    private fun dataForThemeAttribute(attribute: Int): Int {
-        val typedValue = TypedValue()
-        this.theme.resolveAttribute(attribute, typedValue, true)
-
-        return typedValue.data
-    }
-
-    private fun newToolbar(): Toolbar {
-        val toolbar = Toolbar(this)
-        toolbar.layoutParams = ViewGroup.LayoutParams(
-            MATCH_PARENT,
-            TypedValue.complexToDimensionPixelOffset(
-                this.dataForThemeAttribute(R.attr.actionBarSize),
-                this.resources.displayMetrics))
-        // This must be set manually else the toolbar will not be shown.
-        toolbar.setBackgroundColor(this.dataForThemeAttribute(R.attr.colorPrimary))
-
-        return toolbar
-    }
+    } 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val layout = RelativeLayout(this)
-
         this.webView = WebView(this)
-        this.webView.layoutParams = RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.MATCH_PARENT,
-                RelativeLayout.LayoutParams.MATCH_PARENT)
+        this.webView.id = View.generateViewId()
+        val webViewLayoutParams = RelativeLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+        webViewLayoutParams.addRule(RelativeLayout.BELOW, this.toolbar.id)
+        this.webView.layoutParams = webViewLayoutParams
         // Disable scrolling.
         this.webView.setOnTouchListener { _, event -> event.action == MotionEvent.ACTION_MOVE }
         this.webView.loadUrl("file:///android_asset/example.html")
-        layout.addView(this.webView)
-
-        setContentView(layout)
-
-        val toolbar = this.newToolbar()
-        layout.addView(toolbar)
-        this.setSupportActionBar(toolbar)
+        this.layout.addView(this.webView)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
